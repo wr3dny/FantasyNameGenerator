d14f66c785903b694feff76f81a585402d2318e4
Pawe Pawtel <pawel.pawtel@gmail.com>
2023-04-10 20:50:26 +0200

    surnamer done

--- modules/md_surname.py
+++ modules/md_surname.py
@@ -1,12 +1,11 @@
 import random
 from md_parts import vowel_consonant, parts_generator
 surname_prefix = ['Mac', 'Mc', 'O\'', 'al-', 'ibn ']
-surname_suffix = ['son', 'sson']
 
 
 def surname_generator(parts_list):
     generated_surname = []
-    surname_length = random.randint(1, 4)
+    surname_length = random.randint(2, 4)
     k = 0
     for k in range(0, surname_length):
         generated_surname.append(random.choice(parts_list))
@@ -16,19 +15,32 @@ def surname_generator(parts_list):
     surname_out = surname_out.replace(' ', '')
     surname_out = surname_out.capitalize()
 
-    prefix_choice = ['no', 'yes']
-    prefix_yes_no = random.choice(prefix_choice)
-    if prefix_yes_no == 'yes':
-        prefix = random.choice(surname_prefix)
-        surname_out = prefix + surname_out
-    # else:
-    #     surname_out
+    options = ['nothing', 'prefix', 'suffix', 'double', 'double + prefix', 'double + suffix']
 
-    sufix_choice = ['no', 'yes']
-    sufix_yes_no = random.choice(sufix_choice)
+    switch = random.choices(options, weights=[9, 3, 3, 3, 1, 1], k=1)
 
-    if sufix_yes_no == 'yes':
-        if surname_length <= 3:
+    match(switch):
+        case ['nothing']:
+            surname_out = surname_out
+        case ['prefix']:
+            prefix = random.choice(surname_prefix)
+            surname_out = prefix + surname_out
+        case ['suffix']:
+            surname_out = surname_out + 'son'
+            print('case suffix')
+        case ['double']:
+            generated_surname2 = []
+            surname_length = random.randint(2, 3)
+            k = 0
+            for k in range(0, surname_length):
+                generated_surname2.append(random.choice(parts_list))
+                k += 1
+            surname_out2: str = ' '.join(generated_surname2)
+            surname_out2 = surname_out2.replace(' ', '')
+            surname_out2 = surname_out2.capitalize()
+            surname_out = surname_out + '-' + surname_out2
+        case ['double + prefix']:
+            prefix = random.choice(surname_prefix)
             generated_surname2 = []
             surname_length = random.randint(2, 3)
             k = 0
@@ -36,19 +48,20 @@ def surname_generator(parts_list):
                 generated_surname2.append(random.choice(parts_list))
                 k += 1
 
+            surname_out = surname_out + '-' + ' '.join(generated_surname2)
+            surname_out = prefix + surname_out
+        case ['double + suffix']:
+            generated_surname2 = []
+            surname_length = random.randint(2, 3)
+            k = 0
+            for k in range(0, surname_length):
+                generated_surname2.append(random.choice(parts_list))
+                k += 1
             surname_out2: str = ' '.join(generated_surname2)
             surname_out2 = surname_out2.replace(' ', '')
             surname_out2 = surname_out2.capitalize()
-            surname_out = surname_out + '-' + surname_out2
-
-        elif surname_out[1] != 's':
-            surname_out = surname_out + 'sson'
-        else:
-            surname_out = surname_out + 'son'
-    # else:
-    #     surname_out
+            surname_out2 = surname_out + '-' + surname_out2
+            surname_out = surname_out2 + 'son'
 
     return surname_out
 
-
-print(surname_generator(parts_generator()))
